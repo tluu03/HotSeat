6a4ddd0ed8c4b7fd8ec7501799c87397dc3aef13
tluu <tluu03@gmail.com>
2016-12-28 13:52:18 -0500

    clean up

--- app/authentication/models.py
+++ app/authentication/models.py
@@ -2,16 +2,8 @@
 # Import the database object (db) from the main application module
 # We will define this inside /app/__init__.py in the next sections.
 from app import db
+from app.common import Base
 
-# Define a base model for other database tables to inherit
-class Base(db.Model):
-
-    __abstract__  = True
-
-    id            = db.Column(db.Integer, primary_key=True)
-    date_created  = db.Column(db.DateTime,  default=db.func.current_timestamp())
-    date_modified = db.Column(db.DateTime,  default=db.func.current_timestamp(),
-                                           onupdate=db.func.current_timestamp())
 
 # Define User model
 class User(Base):
